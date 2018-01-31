4cf6d74d73c77d628999979d5011b818c2a235b8
Panneerselvam <panneerselvam.nithiyanantham@merrillcorp.com>
2018-01-31 20:02:54 +0530

    TRAIL-0003 Enabling Swagger in product service

--- src/main/java/com/oauthex/productservice/ProductserviceApplication.java
+++ src/main/java/com/oauthex/productservice/ProductserviceApplication.java
@@ -1,11 +1,13 @@
 package com.oauthex.productservice;
 
+import com.oauthex.common.swagger.web.EnableCommonSwagger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCommonSwagger
 public class ProductserviceApplication {
 
 	public static void main(String[] args) {
